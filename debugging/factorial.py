e9cb05fd5eed68cfaeac230623d93f5566f3e3f1
noneals <124405266+noneals@users.noreply.github.com>
2025-11-24 15:35:09 +0300

    Fix indentation for decrement in factorial function

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,7 +5,7 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1   # make the loop terminate
+        n -= 1   
     return result
 
 if __name__ == "__main__":
